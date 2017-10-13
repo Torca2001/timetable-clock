b35a7c2b30b357aef408a937286509722cb33f9a
Josh <116049@trinity.vic.edu.au>
2017-10-13 12:18:57 +1100

    Delete content.js

--- settings/content.js
+++
@@ -1,8 +0,0 @@
-function ldContent() {
-	var settingCategories = ['general', 'classes'];
-}
-
-function setLoc(name) {
-	setLoc = name
-	document.getElementById("windowlocation").innerHTML = setLoc
-}
