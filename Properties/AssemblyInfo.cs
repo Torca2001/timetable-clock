086f870fdeb301eb5c91822810277aeb8f279a47
Josh <116049@trinity.vic.edu.au>
2018-07-19 12:04:02 +1000

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.0.4")]
-[assembly: AssemblyFileVersion("5.0.4")]
+[assembly: AssemblyVersion("5.0.3")]
+[assembly: AssemblyFileVersion("5.0.3")]
