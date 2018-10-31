2980aca71564c503e9a8023468979ffaa1288401
Torca2001 <Torca2001@gmail.com>
2018-10-31 18:29:06 +1100

    Server fix

---
+++ Message.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SchoolManager
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Message));
+            this.OKbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OKbutton
+            // 
+            this.OKbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKbutton.Location = new System.Drawing.Point(202, 263);
+            this.OKbutton.Name = "OKbutton";
+            this.OKbutton.Size = new System.Drawing.Size(86, 25);
+            this.OKbutton.TabIndex = 0;
+            this.OKbutton.Text = "OK";
+            this.OKbutton.UseVisualStyleBackColor = true;
+            this.OKbutton.Click += new System.EventHandler(this.OKbutton_Click);
+            // 
+            // Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.OKbutton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.Name = "Message";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Message";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Message_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OKbutton;
+    }
+}
---
+++ Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManager
+{
+    public partial class Message : Form
+    {
+        public Message()
+        {
+            InitializeComponent();
+            Hide();
+        }
+
+        private void Message_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hide();
+            e.Cancel = (e.CloseReason == CloseReason.UserClosing);  // this cancels the close event.
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            BackgroundImage = null;
+            Width = 300;
+            Height = 300;
+        }
+    }
+}
